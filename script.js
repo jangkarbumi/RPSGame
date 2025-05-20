ce00ddfe4336d754eea2e4c632e4376a47cf18b4
jangkarbumi <wahyuuajigtn13@gmail.com>
2025-05-20 14:46:54 +0700

    Changing the scoreboard from alert to text

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-let score = JSON.parse(localStorage.getItem('score'))
+const menang = document.getElementById('menang');
+const kalah = document.getElementById('kalah');
+const imbang = document.getElementById('imbang');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissor = document.getElementById('scissor');
+const resetScore = document.getElementById('resetScore');
 
-if (score === null) {
-    score = {
-        wins: 0,
-        losses: 0,
-        ties : 0
+let score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+}
+
+function saveScore() {
+    localStorage.setItem('rpsScore', JSON.stringify(score));
+}
+
+function loadScore() {
+    const savedScore = localStorage.getItem('rpsScore');
+    if (savedScore) {
+        score = JSON.parse(savedScore)
     }
 }
 
-function RPS(player){
-    const randomNum = Math.random();
+function RPS(player) {
+    const randomNumber = Math.random();
     let computerMove = '';
 
-    if (randomNum >= 0 && randomNum < 1/3) {
+    if (randomNumber >= 0 && randomNumber <= 1/3) {
         computerMove = 'Rock';
     }
-    else if (randomNum >= 1/3 && randomNum < 2/3) {
+    else if (randomNumber > 1/3 && randomNumber <= 2/3) {
         computerMove = 'Paper';
     }
     else {
@@ -23,70 +38,75 @@ function RPS(player){
     }
 
     let result = '';
-    if (player === 'Rock') {
+    if (player === 'rock') {
         if (computerMove === 'Rock') {
-            result = 'Tie';
+            result = 'tie';
         }
         else if (computerMove === 'Paper') {
-            result = 'You Lose';
+            result = 'lose';
         }
         else {
-            result = 'You Win';
+            result = 'win';
         }
     }
-    else if (player === 'Paper') {
+    else if (player === 'paper') {
         if (computerMove === 'Rock') {
-            result = 'You Win';
+            result = 'win';
         }
         else if (computerMove === 'Paper') {
-            result = 'Tie';
+            result = 'tie';
         }
         else {
-            result = 'Lose';
-        }
+            result = 'lose';
+        }        
     }
     else {
         if (computerMove === 'Rock') {
-            result = 'You Lose';
+            result = 'lose';
         }
-        else if(computerMove === 'Paper') {
-            result = 'You Win';
+        else if (computerMove === 'Paper') {
+            result = 'win';
         }
         else {
-            result = 'Tie';
+            result = 'tie';
         }
     }
 
-    if (result === 'You Win') {
+    if (result === 'win') {
         score.wins += 1;
     }
-    else if (result === 'You Lose') {
+    else if (result === 'lose') {
         score.losses += 1;
     }
     else {
         score.ties += 1;
     }
-
-    localStorage.setItem('score', JSON.stringify(score));
-
-    alert(`You picked ${player}. Computer picked ${computerMove}. ${result}!
-Wins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties}`);
+    saveScore();
 }
 
-document.getElementById('rock').addEventListener('click', function() {
-    RPS('Rock');
-})
-document.getElementById('paper').addEventListener('click', function() {
-    RPS('Paper');
-})
-document.getElementById('scissor').addEventListener('click', function() {
-    RPS('Scissor');
-})
+function updateScore() {
+    menang.textContent = `Wins: ${score.wins}`;
+    kalah.textContent = `Losses: ${score.losses}`;
+    imbang.textContent = `Ties: ${score.ties}`;
+}
 
-document.getElementById('resetScore').addEventListener('click', function() {
-    score.wins = 0;
-    score.losses = 0;
-    score.ties = 0;
-    localStorage.removeItem('score');
-    alert('You reset the score!')
+function reset() {
+    score.wins = score.losses = score.ties = 0;
+    saveScore();
+}
+
+loadScore();
+updateScore();
+
+rock.addEventListener('click', function() {
+    RPS('rock'); updateScore();
+});
+paper.addEventListener('click', function() {
+    RPS('paper'); updateScore();
+});
+scissor.addEventListener('click', function() {
+    RPS('scissor'); updateScore();
+});
+resetScore.addEventListener('click', function() {
+    reset(); updateScore();
 })
